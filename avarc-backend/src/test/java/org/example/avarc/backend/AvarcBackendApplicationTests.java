2617ce0f5c5b2214d13bff4431449dcac9562247
hakan <hakan@tandogan.com>
2024-05-26 18:06:38 +0200

    Every Saga has a... Never mind :)

---
+++ avarc-backend/src/test/java/org/example/avarc/backend/AvarcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.avarc.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvarcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
