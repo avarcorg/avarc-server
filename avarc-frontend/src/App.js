6cabcb0432fc94dea87f815d4c00466d16b43178
hakan <hakan@tandogan.com>
2024-06-14 17:43:11 +0200

    Very trivial frontend-backend communication example
    Signed-off-by: hakan <hakan@tandogan.com>

--- avarc-frontend/src/App.js
+++ avarc-frontend/src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [message, setMessage] = useState('');
+
+  <!-- TODO find a way to configure URL prefixes -->
+  useEffect(() => {
+    fetch('http://localhost:9696/api/hello')
+      .then(response => response.text())
+      .then(data => setMessage(data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
+
+        <p>{message}</p>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
