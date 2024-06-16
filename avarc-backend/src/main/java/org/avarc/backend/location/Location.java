ea60b78689b8526af117b08bfc9b1eda8fb6f5f6
hakan <hakan@tandogan.com>
2024-06-16 19:10:46 +0200

    Keep a human readable presentation of coordinates
    Signed-off-by: hakan <hakan@tandogan.com>

--- avarc-backend/src/main/java/org/avarc/backend/location/Location.java
+++ avarc-backend/src/main/java/org/avarc/backend/location/Location.java
@@ -6,13 +6,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 import org.locationtech.jts.geom.Point;
 
 @Entity
 @Table(name = Location.TABLE_NAME, schema = "public")
+@Data
 public class Location {
 
-    public static final String TABLE_NAME= "LOCATION";
+    public static final String TABLE_NAME = "LOCATION";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,8 +22,10 @@ public class Location {
 
     private String name;
 
+    private Double latitude;
+
+    private Double longitude;
+
     @Column(columnDefinition = "geometry(Point,4326)")
     private Point coordinates;
-
-    // Getters and setters
 }
