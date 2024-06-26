9dfaee940dd290cb5599c2c1555dda98aa94e69e
hakan <hakan@tandogan.com>
2024-06-26 19:25:21 +0200

    Identify locations by name, for now
    Signed-off-by: hakan <hakan@tandogan.com>

--- avarc-backend/src/main/java/org/avarc/backend/location/LocationRepository.java
+++ avarc-backend/src/main/java/org/avarc/backend/location/LocationRepository.java
@@ -3,4 +3,5 @@ package org.avarc.backend.location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
+    Location findByName(String name);
 }
