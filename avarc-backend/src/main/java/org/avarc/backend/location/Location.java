d4e96491e46efc2aa637bc39d06d1bc180f8152c
hakan <hakan@tandogan.com>
2024-06-30 16:25:44 +0200

    Add UUID and description, annotate for usage as JSON document
    Signed-off-by: hakan <hakan@tandogan.com>

--- avarc-backend/src/main/java/org/avarc/backend/location/Location.java
+++ avarc-backend/src/main/java/org/avarc/backend/location/Location.java
@@ -1,13 +1,19 @@
 package org.avarc.backend.location;
 
+import java.util.UUID;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.Data;
 import org.locationtech.jts.geom.Point;
+
 
 @Entity
 @Table(name = Location.TABLE_NAME, schema = "public")
@@ -18,14 +24,31 @@ public class Location {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("id")
     private Long id;
 
+    @Column(nullable = false, unique = true, updatable = false)
+    @JsonProperty(access = Access.READ_ONLY)
+    private UUID guuid;
+
+    @JsonProperty("name")
     private String name;
 
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("latitude")
     private Double latitude;
 
+    @JsonProperty("longitude")
     private Double longitude;
 
     @Column(columnDefinition = "geometry(Point,4326)")
+    @JsonIgnore
     private Point coordinates;
+
+    @PrePersist
+    protected void onCreate() {
+        guuid = UUID.randomUUID();
+    }
 }
