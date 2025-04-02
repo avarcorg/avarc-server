da194e2b26f8fd87e168810b7a3cb55e6e0e25df
Hakan Tandogan <hakan@tandogan.com>
2025-04-02 18:41:19 +0200

    Cleaner package naming
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

--- avarc-backend/src/main/java/org/avarc/server/backend/AvarcBackendApplication.java
+++ avarc-backend/src/main/java/org/avarc/server/backend/AvarcBackendApplication.java
@@ -1,12 +1,17 @@
-package org.avarc.server;
+package org.avarc.server.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 
+@PropertySources(
+    @PropertySource(value = "application-secret.yml", ignoreResourceNotFound = true)
+)
 public class AvarcBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(AvarcBackendApplication.class, args);
