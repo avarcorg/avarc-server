560a9131107cb7b99a964b8725660626d6987846
Hakan Tandogan <hakan@tandogan.com>
2025-04-06 23:30:56 +0200

    Requests are below /rest, Requests are versioned, first POST works
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

--- avarc-backend/src/main/java/org/avarc/server/backend/modules/authentication/api/AuthController.java
+++ avarc-backend/src/main/java/org/avarc/server/backend/modules/authentication/api/AuthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/rest/v0.1.0/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
