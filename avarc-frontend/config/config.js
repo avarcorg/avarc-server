ee339201cef0caeeb831bf9783e96c61e7cc5b93
Hakan Tandogan <hakan@tandogan.com>
2025-04-09 00:15:20 +0200

    Added missing 'config' file
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

---
+++ avarc-frontend/config/config.js
@@ -0,0 +1,24 @@
+export const API_CONFIG = {
+  HOST: process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:8080',
+  VERSION: process.env.NEXT_PUBLIC_API_VERSION || 'v0.1.0',
+};
+
+const BASE_PATH = `/rest/${API_CONFIG.VERSION}`;
+
+export const ENDPOINTS = {
+  AUTH: {
+    BASE: `${BASE_PATH}/auth`,
+    LOGIN: `${BASE_PATH}/auth/login`,
+    REGISTER: `${BASE_PATH}/auth/register`,
+    LOGOUT: `${BASE_PATH}/auth/logout`,
+  },
+  DASHBOARD: {
+    BASE: `${BASE_PATH}/dashboard`,
+    ME: `${BASE_PATH}/dashboard/me`,
+  }
+};
+
+export default {
+  API_CONFIG,
+  ENDPOINTS,
+};
