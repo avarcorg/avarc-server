af53784b1ae3aef6c5dcefb1beed3a66b1a7c303
Hakan Tandogan <hakan@tandogan.com>
2025-04-11 20:08:01 +0200

    Explicit documentation and annotation that passwords shall not pass to the client
    Signed-off-by: Hakan Tandogan <hakan@tandogan.com>

--- avarc-backend/src/main/java/org/avarc/server/backend/modules/user/internal/UserMapper.java
+++ avarc-backend/src/main/java/org/avarc/server/backend/modules/user/internal/UserMapper.java
@@ -7,12 +7,21 @@ import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants.ComponentModel;
 import org.mapstruct.factory.Mappers;
 
+/**
+ * Maps between User entity and UserDto.
+ *
+ * SECURITY NOTE: The password field should never be mapped from Entity to DTO.
+ * If mapping from DTO to Entity is necessary (e.g., for user creation or password change),
+ * this should be explicitly handled in the appropriate controller or service class.
+ */
 @Mapper(componentModel = ComponentModel.SPRING)
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    @Mapping(target = "password", ignore = true)
     UserDto toDto(User user);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "password", ignore = true)
     User toEntity(UserDto dto);
 }
